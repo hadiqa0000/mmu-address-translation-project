1376763a4c07764aebda303e3e3f2aecaeb76d48
Hadiqa <hadiqaimrannn0@gmail.com>
2026-07-09 19:55:35 +0300

    writing kernel module to access RAM

--- kernelspace.c
+++ kernelspace.c
@@ -21,6 +21,26 @@ __section(".gnu.linkonce.this_module") = {
 
 	.arch = MODULE_ARCH_INIT,
 };
-;
+MODULE_INFO(retpoline, "Y");
+
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x2c635209, "module_layout" },
+	{ 0xe8a33f46, "param_ops_ulong" },
+	{ 0x5b8239ca, "__x86_return_thunk" },
+	{ 0xd0da656b, "__stack_chk_fail" },
+	{ 0x87a21cb3, "__ubsan_handle_out_of_bounds" },
+	{ 0x9e9fdd9d, "memunmap" },
+	{ 0x7d628444, "memcpy_fromio" },
+	{ 0x4d924f20, "memremap" },
+	{ 0x92997ed8, "_printk" },
+	{ 0xbdfb6dbb, "__fentry__" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "A0438C0B4BE72A9D0CAD348");
 
 
